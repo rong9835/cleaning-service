4a2586ca9926382733693e5d1fffe4c36aa5f63a
박초롱 <rong9835@naver.com>
2025-04-20 14:38:49 +0900

    feat:헤더,푸터 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter, Abril_Fatface } from 'next/font/google';
 import './globals.css';
-import Header from '@/components/header';
+import Header from '@/components/Header';
+// import Footer from '@/components/Footer';
 
 const inter = Inter({
   variable: '--font-inter',
@@ -28,10 +29,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${abrilFatface.variable} antialiased`}
+        className={`${inter.variable} ${abrilFatface.variable} antialiased flex flex-col min-h-screen`}
       >
         <Header />
-        {children}
+        <main className="flex-1">{children}</main>
         {/* <Footer /> */}
       </body>
     </html>
